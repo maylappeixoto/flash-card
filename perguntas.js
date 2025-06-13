81cd8a9edf2067da1b1d4bc7c34f477520ec4fd1
maylappeixoto <mayy.pxtt@gmail.com>
2025-06-13 11:40:18 +0000

    perguntas

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Beleza',
+    ' Qual o primeiro passo para uma pintura de unha perfeita?',
+    'Limpar e preparar as unhas.
+'
+)
+
+criaCartao(
+    'Igreja',
+    ' Além de ser um local de culto, qual outra função social uma igreja pode ter para a comunidade?',
+    'Pode oferecer apoio social, como ações de caridade ou programas educacionais.
+
+'
+)
+
+criaCartao(
+    'Livros',
+    'Qual o elemento mais comum em livros de romance??',
+    'O desenvolvimento de um relacionamento amoroso.
+'
+)
+
+criaCartao(
+    'Comida',
+    'Além do sabor "umami", quais são os outros quatro gostos básicos que o paladar humano pode identificar?',
+    'Doce, salgado, amargo e azedo.'
+)
